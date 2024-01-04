c26970cac845fdd4dd46601a7bcaaf9c48c40fb6
Danilo <daniilosam23@gmail.com>
2024-01-04 13:14:22 -0300

    initial feature

---
+++ src/main/java/oi/github/D4N23/imageapi/images/controllers/ImagesController.java
@@ -0,0 +1,15 @@
+package oi.github.D4N23.imageapi.images.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/images")
+public class ImagesController {
+
+
+    public ResponseEntity save(){
+
+    }
+}
---
+++ src/test/java/oi/github/D4N23/imageapi/ImageapiApplicationTests.java
@@ -0,0 +1,13 @@
+package oi.github.D4N23.imageapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
