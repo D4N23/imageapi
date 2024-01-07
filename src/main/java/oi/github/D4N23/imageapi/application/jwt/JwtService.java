557d63ce671f70c754d7fb2ac3ff1fb8ed4fa2c4
Danilo <daniilosam23@gmail.com>
2024-01-07 15:30:33 -0300

    feat: implementando a gearção de tokens

--- src/main/java/oi/github/D4N23/imageapi/application/jwt/JwtService.java
+++ src/main/java/oi/github/D4N23/imageapi/application/jwt/JwtService.java
@@ -1,14 +1,51 @@
 package oi.github.D4N23.imageapi.application.jwt;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.crypto.SecretKey;
+
 import org.springframework.stereotype.Service;
 
+import io.jsonwebtoken.Jwts;
+import lombok.RequiredArgsConstructor;
 import oi.github.D4N23.imageapi.domain.AccessToken;
 import oi.github.D4N23.imageapi.domain.entity.User;
 
 @Service
+@RequiredArgsConstructor
 public class JwtService {
-    
+
+    private final SecretKeyGenerator keyGenerator;
+
     public AccessToken generatAccessToken(User user){
-        return new AccessToken("");
+
+        var key = keyGenerator.getKey();
+        var expirationDate = generateExporationDate();
+        var claims =  generateTokenClaims(user);
+
+       String token = Jwts.builder()
+                        .signWith(key)
+                        .subject(user.getEmail())
+                        .expiration(expirationDate)
+                        .claims(claims)
+                        .compact();
+
+        return new AccessToken(token);
+    }
+
+    private Date generateExporationDate(){
+        var expirationMinutes = 60;
+        LocalDateTime now = LocalDateTime.now().plusMinutes(expirationMinutes);
+        return Date.from(now.atZone(ZoneId.systemDefault()).toInstant());
+    }
+
+    private Map<String, Object> generateTokenClaims(User user){
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("name", user.getName());
+        return claims;
     }
 }
---
+++ src/main/java/oi/github/D4N23/imageapi/application/jwt/SecretKeyGenerator.java
@@ -0,0 +1,20 @@
+package oi.github.D4N23.imageapi.application.jwt;
+
+import javax.crypto.SecretKey;
+
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Jwts;
+
+@Component
+public class SecretKeyGenerator {
+
+    private SecretKey key;
+
+    public SecretKey getKey(){
+        if(key == null){
+            key = Jwts.SIG.HS256.key().build();
+        }
+        return key;
+    }
+}
